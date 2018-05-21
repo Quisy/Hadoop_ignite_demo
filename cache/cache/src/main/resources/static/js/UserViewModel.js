54633f544d9b195bb7852c373a662297e8ae0591
Unknown <lamprecht.mariusz@gmail.com>
2018-05-21 10:46:45 +0200

    Added getting data from webhdfs

--- cache/cache/src/main/resources/static/js/UserViewModel.js
+++ cache/cache/src/main/resources/static/js/UserViewModel.js
@@ -5,6 +5,10 @@ var logoutUrl = function () {
     return "/api/user/logout"
 };
 
+var getDataUrl = function() {
+    return "http://192.168.5.11:50075/webhdfs/v1/user/vagrant/output/part-r-00000?op=OPEN&user.name=vagrant&namenoderpcaddress=hadoop-master:8020&offset=0";
+};
+
 
 function UserViewModel() {
     var self = this;
@@ -12,6 +16,7 @@ function UserViewModel() {
     self.userName = ko.observable();
     self.password = ko.observable();
     self.isLoggedIn = ko.observable(false);
+    self.data = ko.observable("");
 
 
     self.loginUser = function () {
@@ -46,8 +51,15 @@ function UserViewModel() {
         });
     };
 
+    self.getData = function() {
+        $.get(getDataUrl(), function (data) {
+            self.data(data);
+        });
+    };
+
     self.init = function () {
         self.checkIfLogged();
+        self.getData();
     };
 
     self.init();
